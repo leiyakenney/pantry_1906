2bb322512d703ec888a2fee555db831e889ac8ea
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-30 10:24:49 -0600

    Works on figuring out nested hash

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -5,6 +5,26 @@ class CookBook
     @summary = {}
   end
 
-  
+  def recipe_hash
+    recipe =
+  end
+
+  def summary
+    cookbook_hash = recipe_hash
+    new_hash = Hash.new
+    cookbook_hash.map do |recipe, sub_hash|
+      new_hash[value] = sub_hash.transform_values do |something|
+      end
+    end
+  end
 
 end
+
+#recipe_hash needs to have:
+  #:name => "name"
+  #:details => {
+    #:ingredients (x2) => [{
+      #:ingredient => "name"
+      #:amount => "recipe.amount_required + ingredient.unit"}]
+    #total_calories => recipe.total_calories
+    #}
