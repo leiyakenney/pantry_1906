ba01de47fb4b2405f8a2b9b766e265239807b733
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-30 10:49:15 -0600

    Attempts to create hash for prior commit's data

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -26,4 +26,11 @@ class Recipe
     ingredients_required[ingredient].to_s + " " + ingredient.unit
   end
 
+  # def real_amount_hash
+  #   real_amt_hash = Hash.new(0)
+  #   @ingredients_required.each do |ingredient, amt|
+  #     real_amt_hash[ingredient.name] = real_amount(ingredient)
+  #   end
+  # end
+
 end
