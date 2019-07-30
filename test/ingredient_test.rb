d8935b8b723a6d0a7258dff8569fcb1a9b7e6c91
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-30 09:05:07 -0600

    Updates syntax

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -4,17 +4,16 @@ require './lib/ingredient'
 
 class IngredientTest < Minitest::Test
 
-  def setup
-    @cheese = Ingredient.new("Cheese", "oz", 50)
-  end
 
   def test_it_exists
-    assert_instance_of Ingredient, @cheese
+    cheese = Ingredient.new("Cheese", "oz", 50)
+    assert_instance_of Ingredient, cheese
   end
 
   def test_attributes
-    assert_equal "Cheese", @cheese.name
-    assert_equal "oz", @cheese.unit
-    assert_equal 50, @cheese.cals
+    cheese = Ingredient.new("Cheese", "oz", 50)
+    assert_equal "Cheese", cheese.name
+    assert_equal "oz", cheese.unit
+    assert_equal 50, cheese.cals
   end
 end
---
+++ test/recipe_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+
+  def setup
+    @cheese = Ingredient.new("Cheese", "oz", 50)
+  end
+end
