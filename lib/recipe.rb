6117d28b86a993b290b80ad158ca822eaba3ed08
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-30 09:22:35 -0600

    Completes getting array of ingredients

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,17 +1,27 @@
 class Recipe
-  attr_reader :name, :ingredients_required
+  attr_reader :name, :ingredients_required, :ingredients
 
   def initialize(name)
     @name = name
     @ingredients_required = {}
+    @ingredients = []
   end
 
   def add_ingredient(ingredient, amount_required)
+    @ingredients << ingredient
     ingredients_required[ingredient] = amount_required
   end
 
   def amount_required(ingredient)
     ingredients_required[ingredient]
   end
+
+  # def total_calories
+  #   total_cals = 0
+  #   @ingredients_required.each do |ingredient, amt|
+  #     total_cals = (ingredient.cals * amt)
+  #   end
+  #   total_cals
+  # end
 
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -35,7 +35,17 @@ class RecipeTest < Minitest::Test
     assert_equal 8, @mac_and_cheese.amount_required(@mac)
   end
 
-  def test_total_calories
+  def test_ingredients
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+    
+    assert_equal [@cheese, @mac], @mac_and_cheese.ingredients
+  end
 
-  end 
+  # def test_total_calories
+  #   @mac_and_cheese.add_ingredient(@cheese, 2)
+  #   @mac_and_cheese.add_ingredient(@mac, 8)
+  #
+  #   assert_equal 440, @mac_and_cheese.total_calories
+  # end
 end
