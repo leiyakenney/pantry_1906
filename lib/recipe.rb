8b72d9fba2772096d82167d59f79a211051eb05d
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-30 09:12:51 -0600

    Completes through amount_required

---
+++ lib/recipe.rb
@@ -0,0 +1,17 @@
+class Recipe
+  attr_reader :name, :ingredients_required
+
+  def initialize(name)
+    @name = name
+    @ingredients_required = {}
+  end
+
+  def add_ingredient(ingredient, amount_required)
+    ingredients_required[ingredient] = amount_required
+  end
+
+  def amount_required(ingredient)
+    ingredients_required[ingredient]
+  end
+
+end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -7,5 +7,35 @@ class RecipeTest < Minitest::Test
 
   def setup
     @cheese = Ingredient.new("Cheese", "oz", 50)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
   end
+
+  def test_it_exists
+    assert_instance_of Recipe, @mac_and_cheese
+  end
+
+  def test_attributes
+    assert_equal "Mac and Cheese", @mac_and_cheese.name
+    assert_equal ({}), @mac_and_cheese.ingredients_required
+  end
+
+  def test_add_ingredients
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    assert_equal ({@cheese => 2, @mac => 8}), @mac_and_cheese.ingredients_required
+  end
+
+  def test_amount_required
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    assert_equal 2, @mac_and_cheese.amount_required(@cheese)
+    assert_equal 8, @mac_and_cheese.amount_required(@mac)
+  end
+
+  def test_total_calories
+
+  end 
 end
